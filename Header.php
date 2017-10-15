22464af4827baff2c2095f6a8eb38ae074f35af5
VincentROUSSET <vincent601998@outlook.fr>
2017-10-15 12:54:08 +0200

    Add files via upload

---
+++ Header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Playfair+Display:400italic" />
+	<meta charset="UTF-8">
+</head>
+<body>
+<div class="blocLogo">
+	<img src="img/logoSV.png">
+	<p><div class="text0">Créer, partager, construire ensemble</div><br><div class="text1">Lycée Saint-Vincent</div><br><div class="text2">SUAVITER & FORTITER</div></p>
+</div>
+
+<section id="nav-test">
+  <div id="nav-container">
+    <ul>
+      <div class="lesLi">
+          <li class="nav-li active-nav"><a href="accueil.php">Accueil</a></li>
+          <li class="nav-li"><a href="programme.php">Programme</a></li>
+          <li class="nav-li"><a href="live.php">Live</a></li>
+          <li class="nav-li"><a href="retour.php">Retour</a></li>
+      </div>
+    </ul>
+    <div id="line"></div>
+  </div>
+</section>
+</body>
+</html>
+  
+
